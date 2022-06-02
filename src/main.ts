4be2b95b3006feacca373c60151f6dd3b53c9167
AzureAD\DenNg <ben.pmt2d@gmail.com>
2022-06-02 14:57:13 +0800

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // default/other underlying http servers have type extends INestApplication, no need to state if no use
+  const app = await NestFactory.create<INestApplication>(AppModule);
+
+  // documentation
+  // https://docs.nestjs.com/openapi/introduction
+  SwaggerModule.setup(
+    '/docs',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder().setTitle("Documentation for nest playground.").build(),
+    )
+  );
+
   await app.listen(3000);
 }
 bootstrap();
