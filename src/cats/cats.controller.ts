e3a2d8221037daf9532c843dcd8781522a8f8ff7
AzureAD\DenNg <ben.pmt2d@gmail.com>
2022-06-02 15:49:17 +0800

    clean up

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,51 +1,10 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Query, Redirect, Req, Res } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
-import { Request, Response } from 'express';
-import { CreateCatDto } from './dtos/cats.dto';
+import { Controller, Get } from '@nestjs/common';
 
+// https://docs.nestjs.com/controllers
 @Controller('cats')
 export class CatsController {
-    // https://docs.nestjs.com/controllers#routing
-    @Get("response/std")
-    @ApiOperation({ summary: "Demonstrate standard response manipulation." })
-    private standardResponse() {
+    @Get()
+    private root() {
         return "CATS!";
-    }
-
-    @Get("response/lib")
-    @ApiOperation({ summary: "Demonstrate lib-specific response manipulation." })
-    private libSpecificResponse(@Res() response: Response) {
-        response.status(200).send("CATS!");
-    }
-
-    // https://docs.nestjs.com/controllers#request-object
-    @Post("requestObject/:id")
-    @ApiOperation({ summary: "Demonstrate request obj access." })
-    private reqExtraction(
-        @Param("id") id: string,
-        @Query("qField") qField: string,
-    ) {
-        return { id, qField };
-    }
-
-    // https://docs.nestjs.com/controllers#status-code
-    @Get("statusCodeOverride")
-    @HttpCode(HttpStatus.I_AM_A_TEAPOT)
-    @ApiOperation({ summary: "Demonstrate status code override." })
-    private statusCodeOverride() {
-        return true;
-    }
-
-    // https://docs.nestjs.com/controllers#redirection
-    @Get("redirect")
-    @Redirect("/cats/statusCodeOverride") // default, can be overriden by controller return
-    @ApiOperation({ summary: "Demonstrate redirection." })
-    private redirect() { }
-
-    // https://docs.nestjs.com/controllers#redirection
-    @Get("dtos")
-    @ApiOperation({ summary: "Demonstrate dtos." })
-    private dtos(createCatDto: CreateCatDto) {
-        return true;
     }
 }
